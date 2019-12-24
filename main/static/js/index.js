b098ca17a8f3cc98cf263f31243c73605ca3feab
mayconabe <maycon.abe01@gmail.com>
2019-12-24 03:50:36 +0000

    index

--- main/static/js/index.js
+++ main/static/js/index.js
@@ -1,5 +1,27 @@
-$('.carousel').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 3
-  });
+jQuery(document).ready(function($) {
+
+    $("#setaEsquerda").click(function(){
+        $('#scroller').animate( { scrollLeft: '-=1000' }, 500);
+    });
+
+    $("#setaDireita").click(function(){
+        $('#scroller').animate( { scrollLeft: '+=1000' }, 500);
+    });
+
+    $("#setaEsquerda2").click(function(){
+        $('#scroller2').animate( { scrollLeft: '-=1000' }, 500);
+    });
+
+    $("#setaDireita2").click(function(){
+        $('#scroller2').animate( { scrollLeft: '+=1000' }, 500);
+    });
+
+    $("#setaEsquerda3").click(function(){
+        $('#scroller3').animate( { scrollLeft: '-=1000' }, 500);
+    });
+
+    $("#setaDireita3").click(function(){
+        $('#scroller3').animate( { scrollLeft: '+=1000' }, 500);
+    });
+
+});
